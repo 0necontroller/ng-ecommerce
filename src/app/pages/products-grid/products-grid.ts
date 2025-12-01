36ecaeb18e6c15b0a9f8868736876e096fe9d0b6
Abel Misiocha <wells.mail.13@gmail.com>
2025-12-01 10:18:39 +0300

    feat(core): Add individual category products page

--- src/app/pages/products-grid/products-grid.ts
+++ src/app/pages/products-grid/products-grid.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'
+import { Component, inject } from '@angular/core'
+import { toSignal } from '@angular/core/rxjs-interop'
+import { CategoryService } from '../../services/category'
+import { ProductCard } from '../../components/products/product-card/product-card'
 
 @Component({
   selector: 'app-products-grid',
-  imports: [],
+  imports: [ProductCard],
   templateUrl: './products-grid.html',
   styles: ``,
 })
 export default class ProductsGrid {
+  private api = inject(CategoryService)
+
+  private activatedRoute = inject(ActivatedRoute)
+  private categoryId = +this.activatedRoute.snapshot.paramMap.get('categoryId')!
+
+
+  products = toSignal(this.api.fetchCategoryItems(this.categoryId), { initialValue: [] })
 
 }
