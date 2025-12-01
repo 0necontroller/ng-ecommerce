a1bd44afc3602969abf8ef98304460112e174432
Abel Misiocha <wells.mail.13@gmail.com>
2025-12-01 20:45:32 +0300

    feat(core): Added state check for product details page

--- src/app/components/products/product-details/product-details.ts
+++ src/app/components/products/product-details/product-details.ts
@@ -1,14 +1,40 @@
-import { Component, input } from '@angular/core';
-import { MatButton } from '@angular/material/button';
-import { IProduct } from '../../../models/product.model';
+import { Router } from '@angular/router'
+import { MatIcon } from '@angular/material/icon'
+import { IProduct } from '../../../models/product.model'
+import { EcommerceStore } from '../../../store/ecommerce-store'
+import { Component, computed, inject, input } from '@angular/core'
+import { MatButton, MatIconButton } from '@angular/material/button'
 
 @Component({
   selector: 'app-product-details',
-  imports: [MatButton],
+  imports: [MatButton, MatIconButton, MatIcon],
   templateUrl: './product-details.html',
   styles: ``,
 })
 export class ProductDetails {
   productData = input.required<IProduct>()
 
+  private ecommerceStore = inject(EcommerceStore)
+  private router = inject(Router)
+
+  addToCart(product: IProduct) {
+    if (this.isInCart()) {
+      this.router.navigate(['/cart'])
+      return
+    }
+
+    this.ecommerceStore.addToCart(product)
+  }
+
+  toggleWishlist(product: IProduct) {
+    this.ecommerceStore.toggleWishlist(product)
+  }
+
+  isInWishlist = computed(() =>
+    this.ecommerceStore.wishlist().some((item) => item.id === this.productData().id),
+  )
+
+  isInCart = computed(() =>
+    this.ecommerceStore.cart().some((item) => item.id === this.productData().id),
+  )
 }
